015d92f11b52f84e886d8283815a7feaf181be1b
yeong132 <pfyrmog6964@gmail.com>
2025-04-30 19:52:33 +0900

    새로운 액터 클래스 추가
    팀원이 커밋과 푸시한 상황을 훈련하기 위해 새로운 액터 클래스 생성

---
+++ Source/PracticeGit1/GitTestActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GitTestActor.h"
+
+// Sets default values
+AGitTestActor::AGitTestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGitTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGitTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
